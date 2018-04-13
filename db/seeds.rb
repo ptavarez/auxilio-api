8f546629c9bac4e748466a19861f52fc42a9cf61
ptavarez16 <ptavarez16@yahoo.com>
2018-04-13 19:52:58 -0400

    Add seed to create services Seed file creates services from data/services.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database
-# with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the
-# db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('data', 'services.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  row = row.to_hash
+  s = Service.new
+  s.name = row['name']
+  s.description = row['description']
+  s.category = row['category']
+  s.save
+  puts "#{s.name} saved"
+end
+
+puts "There are now #{Service.count} rows in the service table"
