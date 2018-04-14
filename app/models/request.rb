a0dc81d2caa48ae869f911590216dd84c334b053
ptavarez16 <ptavarez16@yahoo.com>
2018-04-14 11:10:40 -0400

    Rename request resource
    Issue: Was getting a little confused with the naming of the resource.
    Seeing requests all over the place was getting to me lol.
    
    Destroy scaffold request.

--- app/models/request.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Request < ApplicationRecord
-  before_save :default_fufilled
-
-  def default_fufilled
-    self.fulfilled ||= false
-  end
-end
--- app/serializers/request_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :service, :fulfilled
-end
