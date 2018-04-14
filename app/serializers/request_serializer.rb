b9c559e8233be3ae3d6624fc76b8b57e04375b8c
ptavarez16 <ptavarez16@yahoo.com>
2018-04-13 20:55:16 -0400

    Add editable Only owned user will be able to edit requests

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,9 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :fulfilled, :date
+  attributes :id, :fulfilled, :date, :editable
   has_one :user
   has_one :service
+
+  def editable
+    scope == object.user
+  end
 end
