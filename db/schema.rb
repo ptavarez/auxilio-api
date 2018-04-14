b187ca71dbf3aaf8b7678d9f630cce2b4caca673
ptavarez16 <ptavarez16@yahoo.com>
2018-04-13 20:27:20 -0400

    Create request migration Ran rails db:migrate to migrate request resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413194222) do
+ActiveRecord::Schema.define(version: 20180414001700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20180413194222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.boolean "fulfilled"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "services", force: :cascade do |t|
