2d6904e410624a64a5df86fb98d8587afd186508
ptavarez16 <ptavarez16@yahoo.com>
2018-04-16 14:32:47 -0400

    Add :service_id
    - Add :service_id to serializer to return service_id data

--- app/serializers/assist_serializer.rb
+++ app/serializers/assist_serializer.rb
@@ -1,5 +1,5 @@
 class AssistSerializer < ActiveModel::Serializer
-  attributes :id, :service, :fulfilled, :editable, :created_at
+  attributes :id, :service_id, :service, :fulfilled, :editable, :created_at
 
   def editable
     scope == object.user
