5f7a40fbcfe2373de8d7f5094a5b7b41b439fbea
ptavarez16 <ptavarez16@yahoo.com>
2018-04-19 13:08:50 -0400

    Add route to search for services

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show update]
 
   # Custom routes
+  get 'search(/:search)' => 'services#search_services'
+
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
