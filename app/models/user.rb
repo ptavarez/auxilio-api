c6ee8a0903b3594cf08974e53bd47b880968d5a3
ptavarez16 <ptavarez16@yahoo.com>
2018-05-12 18:57:20 -0400

    Driver default to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :assists
+
+  before_save :default_driver
+
+  def default_driver
+    self.driver ||= false
+  end
 end
