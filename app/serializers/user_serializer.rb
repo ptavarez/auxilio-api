4055ce23f790abff2cdae73efc35c8282bb3f07b
ptavarez16 <ptavarez16@yahoo.com>
2018-05-12 18:55:54 -0400

    Add driver to user
    - Add driver to serializer to access data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at
+  attributes :id, :email, :driver, :created_at
   has_many :assists
 end
